a99f81b6e442d3241125e6ae463c7ed8fb9c959a
Barry Commins <barry.x.commins@aib.ie>
2016-10-12 16:39:12 +0100

    Added Cassandra as a backend for Zipkin

--- README.md
+++ README.md
@@ -15,3 +15,5 @@ The ```docker-compose.yml``` file contains the two applications, Kafka and Zooke
 It can be run by first building the application containers with ```mvn clean install``` and then running everything with ```docker-compose up```
 
 ```$DOCKER_HOST_IP``` should be set to the ip of your docker machine if you are using Windows or Mac, or to the host ip on Linux.
+
+```zikpkinserver``` is configured to use Cassandra (version 3) as a backend when using Docker
--- zipkinserver/src/main/java/com/barrycommins/ZipkinserverApplication.java
+++ zipkinserver/src/main/java/com/barrycommins/ZipkinserverApplication.java
@@ -2,13 +2,14 @@ package com.barrycommins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
 import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {CassandraAutoConfiguration.class})
 @EnableZipkinStreamServer
-public class ZipkinserverApplication {
+public class ZipkinServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZipkinserverApplication.class, args);
+		SpringApplication.run(ZipkinServerApplication.class, args);
 	}
 }
